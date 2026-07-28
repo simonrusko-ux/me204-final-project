7f7b00344b12bfd20d89904bc16cc6411b66fcd6
Simon Rusko <simon.rusko@gmail.com>
2026-07-29 00:48:17 +0100

    putting the data into csv and finalising part 02

--- scripts/NB02-Data-Transformation.py
+++ scripts/NB02-Data-Transformation.py
@@ -22,21 +22,46 @@ def get_mean_month(df: pd.DataFrame) -> pd.DataFrame:
     df["value"] = pd.to_numeric(df["value"])
     df["date"] = df["date"].str[:7]
 
-    df_mean_montly = (
+    monthly = (
         df.groupby(["series", "date"])["value"].mean().reset_index()
     )
 
-    return df_mean_montly
+    return monthly
 
 
+def transform_pct_sp500() -> pd.DataFrame:
+
+    df_sp500 = load_data_rows("SP500")
+    df_mean_sp500 = get_mean_month(df_sp500)
+
+    df_mean_sp500["value"] = df_mean_sp500["value"].pct_change() * 100
+    df_mean_sp500 = df_mean_sp500.dropna(subset="value")
+
+    df_mean_sp500 = df_mean_sp500.rename(columns={"value":"return_pct"})
+    return df_mean_sp500
 
 
+def transform_pct_brmsa() -> pd.DataFrame:
 
-df = load_data_rows("BRMSA0104")
+    df_brmsa0104 = load_data_rows("BRMSA0104")
+    df_mean_brmsa0104 = get_mean_month(df_brmsa0104)
 
-get = get_mean_month(df)
+    df_mean_brmsa0104["value"] = df_mean_brmsa0104["value"] / 12
 
-print(get)
+    df_mean_brmsa0104 = df_mean_brmsa0104.rename(columns={"value":"return_pct"})
+    return df_mean_brmsa0104
 
 
+def put_to_csv():
 
+    brmsa0104 = transform_pct_brmsa()
+    sp500 = transform_pct_sp500()
+
+    df = pd.concat([brmsa0104, sp500])
+    df = df.drop(index=0)
+
+    with open("data/processed/monthly_returns.csv","w",encoding="utf-8") as f:
+        df.to_csv(f, index=False)
+
+
+put_to_csv()
